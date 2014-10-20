af40d945428063312da1fa3695f7776bf950fa24
azam alias <azam.alias@iproperty.com>
2014-10-20 17:05:08 +0800

    modified admin panel for easier data entry, with inline patterns per app

--- batch_apps/admin.py
+++ batch_apps/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from batch_apps.models import App, Pattern
 
-admin.site.register(App)
-admin.site.register(Pattern)
+
+class PatternInline(admin.StackedInline):
+    model = Pattern
+    extra = 0
+
+
+class AppAdmin(admin.ModelAdmin):
+    fields = ['name', 'is_active']
+    inlines = [PatternInline]
+
+admin.site.register(App, AppAdmin)
