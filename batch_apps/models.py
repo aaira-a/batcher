51fc47375714ec8438adcc4e9139ad022d450311
azam alias <azam.alias@iproperty.com>
2014-10-20 16:36:02 +0800

    added readable string representation for both models for viewing in admin panel

--- batch_apps/models.py
+++ batch_apps/models.py
@@ -6,9 +6,15 @@ class App(models.Model):
     name = models.CharField(max_length=500)
     is_active = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name
+
 
 class Pattern(models.Model):
 
     app = models.ForeignKey(App)
     pattern_string = models.CharField(max_length=500)
     is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.pattern_string
