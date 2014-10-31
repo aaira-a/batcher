66f18671820d99be2246b0928075f860821814e1
azam alias <azam.alias@iproperty.com>
2014-10-31 09:25:26 +0800

    added simple day object creation. to be moved to test_models.py

--- batch_apps/generator.py
+++ batch_apps/generator.py
@@ -1,7 +1,13 @@
+from batch_apps.models import Day
 import datetime
 import pytz
 
 
-def get_current_date_in_gmt8(self):
+def get_current_date_in_gmt8():
     current_datetime = datetime.datetime.now(pytz.timezone('Asia/Kuala_Lumpur'))
     return current_datetime.date()
+
+
+def create_day_object(date_):
+    day = Day.objects.create(date=date_)
+    return day
--- batch_apps/tests/test_generator.py
+++ batch_apps/tests/test_generator.py
@@ -8,4 +8,8 @@ class GenerateDayTest(TestCase):
     def test_get_current_date_in_gmt8(self):
         datetime_utc = datetime.datetime.now(pytz.utc)
         converted_datetime = datetime_utc.astimezone((timezone('Asia/Kuala_Lumpur')))
-        self.assertEqual(get_current_date_in_gmt8(self), converted_datetime.date())
+        self.assertEqual(get_current_date_in_gmt8(), converted_datetime.date())
+
+    def test_create_day_object_for_today_in_gmt8(self):
+        day = create_day_object(get_current_date_in_gmt8())
+        self.assertEqual(day.date, get_current_date_in_gmt8())
