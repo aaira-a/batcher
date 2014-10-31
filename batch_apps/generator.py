01efe818dab7ef21a2f2ac5048b9c69a101bebf1
azam alias <azam.alias@iproperty.com>
2014-10-31 10:51:21 +0800

    added execution object creation. to be moved to test_models.py

--- batch_apps/generator.py
+++ batch_apps/generator.py
@@ -1,4 +1,4 @@
-from batch_apps.models import Day
+from batch_apps.models import Day, Execution
 import datetime
 import pytz
 
@@ -11,3 +11,13 @@ def get_current_date_in_gmt8():
 def create_day_object(date_):
     day, is_new = Day.objects.get_or_create(date=date_)
     return day
+
+
+def create_execution_object(day_, app_):
+
+    if app_.is_active is True:
+        execution, is_new = Execution.objects.get_or_create(day=day_, app=app_)
+        return execution
+
+    else:
+        return None
--- batch_apps/tests/test_generator.py
+++ batch_apps/tests/test_generator.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from batch_apps.generator import *
+from batch_apps.models import App
 from pytz import timezone
 
 
@@ -18,3 +19,19 @@ class GenerateDayTest(TestCase):
         day_existing = create_day_object(get_current_date_in_gmt8())
         day_new = create_day_object(get_current_date_in_gmt8())
         self.assertEqual(day_existing, day_new)
+
+
+class GenerateExecutionTest(TestCase):
+
+    def test_create_execution_object_for_an_app_for_today(self):
+        day = create_day_object(get_current_date_in_gmt8())
+        app = App.objects.create(name='My App 001', is_active=True)
+        execution = create_execution_object(day, app)
+        self.assertEqual(execution.app.name, 'My App 001')
+        self.assertEqual(execution.day.date, get_current_date_in_gmt8())
+
+    def test_create_execution_object_for_inactive_app_should_return_none(self):
+        day = create_day_object(get_current_date_in_gmt8())
+        app = App.objects.create(name='My Inactive App 001', is_active=False)
+        execution = create_execution_object(day, app)
+        self.assertIsNone(execution)
