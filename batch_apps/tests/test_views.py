99d1579d07b3df9d04b732ff3ee2201a4695a2a2
azam alias <azam.alias@iproperty.com>
2014-11-07 10:06:28 +0800

    fixed greedy url config for base execution view

--- batch_apps/tests/test_views.py
+++ batch_apps/tests/test_views.py
@@ -7,6 +7,10 @@ class ExecutionsViewTest(TestCase):
         response = self.client.get('/executions/')
         self.assertTemplateUsed(response, 'executions.html')
 
+    def test_view_should_return_404_if_there_is_unspecified_trailing_characters(self):
+        response = self.client.get('/executions/1wh4t3v3r')
+        self.assertEqual(response.status_code, 404)
+
     def test_url_specific_execution_date_renders_execution_template(self):
         response = self.client.get('/executions/2014-11-03/')
         self.assertTemplateUsed(response, 'executions.html')
