fcd63ebd5010e00231c148d1e2ea7bddaf61329d
azam alias <azam.alias@iproperty.com>
2014-11-11 18:40:18 +0800

    added weekly view for executions. still wip

--- batch_apps/tests/test_views.py
+++ batch_apps/tests/test_views.py
@@ -23,6 +23,33 @@ class ExecutionsViewTest(TestCase):
 
     def test_execution_view_should_return_404_for_date_more_than_today(self):
         today = get_current_date_in_gmt8()
-        tomorrow = datetime.date(today.year, today.month, today.day + 1)
+        tomorrow = today + datetime.timedelta(days=1)
         response = self.client.get('/executions/' + tomorrow.strftime("%Y-%m-%d") + '/')
+        self.assertEqual(response.status_code, 404)
+
+
+class WeeklyExecutionsViewTest(TestCase):
+
+    def test_weekly_executions_view_renders_executions_template(self):
+        response = self.client.get('/executions/week/2014-10-30/')
+        self.assertTemplateUsed(response, 'executions_week.html')
+
+    def test_weekly_view_should_return_404_if_there_is_unspecified_trailing_characters(self):
+        response = self.client.get('/executions/week/2014-10-30/extra')
+        self.assertEqual(response.status_code, 404)
+
+    def test_weekly_execution_view_should_render_for_correct_date_context(self):
+        response = self.client.get('/executions/week/2014-10-30/')
+        self.assertContains(response, "Oct. 24, 2014")
+        self.assertContains(response, "Oct. 25, 2014")
+        self.assertContains(response, "Oct. 26, 2014")
+        self.assertContains(response, "Oct. 27, 2014")
+        self.assertContains(response, "Oct. 28, 2014")
+        self.assertContains(response, "Oct. 29, 2014")
+        self.assertContains(response, "Oct. 30, 2014")
+
+    def test_weekly_execution_view_should_return_404_for_date_more_than_today(self):
+        today = get_current_date_in_gmt8()
+        tomorrow = today + datetime.timedelta(days=1)
+        response = self.client.get('/executions/week/' + tomorrow.strftime("%Y-%m-%d") + '/')
         self.assertEqual(response.status_code, 404)
--- batch_apps/views.py
+++ batch_apps/views.py
@@ -29,3 +29,25 @@ def specific_date(request, yyyy_mm_dd):
 
         context = {'date': date_, 'executions_list': executions_list}
         return render(request, 'executions.html', context)
+
+
+def one_week_view(request, yyyy_mm_dd):
+
+    today = get_current_date_in_gmt8()
+    date_ = datetime.datetime.strptime(yyyy_mm_dd, "%Y-%m-%d").date()
+
+    if date_ > today:
+        return HttpResponseNotFound("<h1>Page not found - Can not show date more than today</h1>")
+
+    else:
+        dates = generate_one_week_date(yyyy_mm_dd)
+
+        for date in dates:
+            day_object = get_or_create_day_object(date)
+            active_apps = App.objects.filter(is_active=True)
+            get_or_create_execution_objects(day_object, active_apps)
+
+        executions_list = Execution.objects.filter(day__date=dates[0])
+
+        context = {'dates': dates, 'executions_list': executions_list}
+        return render(request, 'executions_week.html', context)
