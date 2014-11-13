d614bff2bc6a0f0ef68a6aea1b2bf767fdd9d589
azam alias <azam.alias@iproperty.com>
2014-11-13 12:10:18 +0800

    moved cell colouring into separate js file, unit tested using qunit

---
+++ batch_apps/static/cells.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $(".is_due_True.is_executed_True").addClass("green");
+    $(".is_due_True.is_executed_False").addClass("red");
+    $(".is_due_False.is_executed_True").addClass("yellow");
+    $(".is_due_False.is_executed_False").addClass("gray");
+});
