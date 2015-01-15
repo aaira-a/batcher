a6dc052bae5ed1d6a05de0b38ffac7666901f335
azam alias <azam.alias@iproperty.com>
2015-01-15 14:37:34 +0800

    added patching script for django_mailbox, with hardcoded virtualenv path

---
+++ patches/patch.bat
@@ -0,0 +1,3 @@
+del /Q ..\batcher-virtualenv\Lib\site-packages\django_mailbox\migrations\0*.*
+copy django_mailbox\migrations\ ..\batcher-virtualenv\Lib\site-packages\django_mailbox\migrations\
+copy django_mailbox\*.py ..\batcher-virtualenv\Lib\site-packages\django_mailbox\*.py
